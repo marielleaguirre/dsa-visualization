b4852ebf7669b5b39b92649faf57059864129019
Karl <romerokarl434@gmail.com>
2025-12-21 00:06:19 +0800

    docs: changed file name of binary tree

---
+++ binary_tree.py
@@ -0,0 +1,26 @@
+'''
+Binary Tree Pseudocode
+
+Key Classes:
+- BinaryTree > generates an empty binary tree and will store the nodes produced by Nodes
+- Nodes > handles the creation of left/right nodes
+- Traversals > creates the traversal equivalents of the binary tree
+
+
+'''
+
+class BinaryTree:
+    pass
+
+
+class Nodes:
+    pass
+
+
+class Traversals:
+    pass
+
+
+
+ask_depth = int(input("How many levels would you like the binary tree to have? (Pick 1-5)"))
+gen_tree(ask_depth)
