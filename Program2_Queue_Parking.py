574fc34e13069b591b36b32a7b5188200e129e43
jasmintanyag <tanyagjasmin@gmail.com>
2025-12-21 22:55:40 +0800

    added pseudocode foe queue parking garage

---
+++ Program2_Queue_Parking.py
@@ -0,0 +1,24 @@
+'''
+Queue Parking Garage Similation Pseudocode:
+1. Create a menu with options to:
+    a. Park a car
+    b. Remove a car
+    c. View parked cars
+    d. Exit the program
+2. Ask the user to select an option from the menu.
+3. If the user selects "Park a car":
+    a. Check if there is available space in the parking garage.
+    b. If space is available, prompt the user to enter the car's license plate number.
+    c. Add the car to the parking queue.
+    d. Confirm that the car has been parked.
+4. If the user selects "Remove a car":
+    a. Check if there are any cars parked in the garage.
+    b. If there are parked cars, prompt the user to enter the car's license plate number.
+    c. Remove the car from the parking queue.
+    d. Confirm that the car has been removed.
+5. If the user selects "View parked cars":
+    a. Display a list of all parked cars.
+6. If the user selects "Exit":
+    a. End the program.
+'''
+
