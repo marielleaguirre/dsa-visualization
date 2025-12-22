5adae18295fa2a8ef45f3413f0eeb5f1921dc5e7
DyrenJmsu <darenjamesdedios@gmail.com>
2025-12-22 22:46:43 +0800

    Added value checker which checks if the garage/stack is empty or full

--- Program1_Stacks_Parking_Garage.py
+++ Program1_Stacks_Parking_Garage.py
@@ -14,18 +14,26 @@ Stacks Parking Garage PseudoCode:
  3. Create a program that organizes the Car using Stacks
     *LIFO(Last in First Out)
 """
+
 class ParkingGarageStacks:
     def __init__(self):
         self.garage_capacity = 10       #The capacity of the garage
         self.stack = []                 #Set a variable into an empty list
+        self.occupied = 0
 
     def park(self, car):
-        self.stack.append(car)          #Inserting the car into the garage capacity
+        self.occupied += 1              
+        if self.occupied >= self.garage_capacity:        #Checks if the garage is full
+          pass
+        else:
+          self.stack.append(car)          #Inserting the car into the garage capacity
         
     def depart(self, car):
-        self.stack.pop(car)             #Removing the car from the garage capacity
-
-
+        if not self.stack:                #Checks if the garage is empty
+          self.stack.pop(car)               #Removing the car from the garage capacity
+     
+    def view_car():
+        pass
     
-        
+    
 
