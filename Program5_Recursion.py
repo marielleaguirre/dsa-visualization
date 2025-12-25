08021c889e2b1faeaef3ea86cf55c11a351b0c51
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-12-25 14:05:31 +0800

    chore: remove pseudocode from project

--- Program5_Recursion.py
+++ Program5_Recursion.py
@@ -1,16 +1,3 @@
-'''
-Recursion(Tower of Hanoi) Pseudocode:
-1. Ask the user to enter the number of disks.
-2. Define a recursive function `tower_of_hanoi(n, source, auxiliary, target)`:
-    a. If n == 1:
-        i. Print "Move disk 1 from source to target"
-        ii. Return
-    b. Move n-1 disks from source to auxiliary using target as auxiliary
-    c. Print "Move disk n from source to target"
-    d. Move n-1 disks from auxiliary to target using source as auxiliary
-3. Call `tower_of_hanoi` with the number of disks, source = A, auxiliary = B, target = C
-'''
-
 def tower_of_hanoi(num_disks, source, auxiliary, target):
     if num_disks == 1:
         print(f"Move disk 1 from {source} to {target}")  # Base case: move one disk directly to target
