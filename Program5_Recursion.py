da83018085dd7d3edd6fd96e3b04ab2c2cb70ee6
marielleaguirre <mariellemaebaguirre@iskolarngbayan.pup.edu.ph>
2025-12-25 13:38:42 +0800

    feat: validate Tower of Hanoi disk count between 1 and 7

--- Program5_Recursion.py
+++ Program5_Recursion.py
@@ -10,3 +10,14 @@ Recursion(Tower of Hanoi) Pseudocode:
     d. Move n-1 disks from auxiliary to target using source as auxiliary
 3. Call `tower_of_hanoi` with the number of disks, source = A, auxiliary = B, target = C
 '''
+
+def main():
+    print("Recursion: Tower of Hanoi")
+    while True:
+        num_disks = int(input("Enter the number of disks (1-7): "))
+        if 1 <= num_disks <= 7:
+            break
+        else:      
+            print("Invalid. Please enter a valid number between 1 and 7.")
+
+main()
