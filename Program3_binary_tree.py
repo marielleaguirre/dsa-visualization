c2b1e114cf0cf0e381eb0476fcdc07e7eccfcc5b
Karl <romerokarl434@gmail.com>
2025-12-31 16:13:18 +0800

    feat: Created a class template to validate user inputs

--- Program3_binary_tree.py
+++ Program3_binary_tree.py
@@ -167,20 +167,6 @@ class Traversal:
         return result
 
 
-class ValidateInput: # Still working on it
-
-    def validate(prompt, input_limit=[]):
-        while True:
-            try:
-                user_input = input(prompt).strip()
-
-                if user_input in input_limit or input_limit == []:
-                    return user_input
-
-            except ValueError:
-                print("Invalid Input!")
-
-
 def main():
     border = 20 * "="
     print(border, "BINARY TREE GENERATOR", border)
