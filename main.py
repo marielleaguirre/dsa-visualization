76928108ce7acaadf7d873aa1fb5112a4ad822a4
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2026-01-20 16:38:33 +0800

    imported the modules from src to main program

---
+++ main.py
@@ -0,0 +1,7 @@
+from src import Program1_Stacks_Parking_Garage, Program2_Queue_Parking_Garage, binary_tree_gui, Program4_Binary_Search_Tree, Program5_Recursion
+
+Program1_Stacks_Parking_Garage.main()
+Program2_Queue_Parking_Garage.main()
+binary_tree_gui.main()
+Program4_Binary_Search_Tree.main()
+Program5_Recursion.main()
