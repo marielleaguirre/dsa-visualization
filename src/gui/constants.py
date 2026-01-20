d4d75290f11cfc098834c18a7e3a46a5e64f4b64
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2026-01-20 15:37:09 +0800

    doc: make constants file for documentation

---
+++ src/gui/constants.py
@@ -0,0 +1,16 @@
+# Program 3 -Binary Tree Constants
+
+COLOR_BACKGROUND = (20, 20, 20)  # Dark grey/black
+COLOR_NODE = (240, 240, 240)     # White
+COLOR_NODE_BORDER = (100, 100, 100)  # Grey
+COLOR_NONE_NODE = (60, 60, 60)   # Dark grey
+COLOR_TEXT = (240, 240, 240)     # White
+COLOR_CONNECTOR = (150, 150, 150)  # Light grey
+COLOR_INPUT_BG = (40, 40, 40)    # Dark grey
+COLOR_INPUT_BORDER = (120, 120, 120)  # Grey
+COLOR_HIGHLIGHT = (100, 150, 255)  # Light blue for active input
+
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+
+NODE_RADIUS = 25
