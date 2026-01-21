e5aea955f4ba6edd12067bd07f547e4a44520a30
DyrenJmsu <darenjamesdedios@gmail.com>
2026-01-21 21:29:49 +0800

    feat: resizing to fullscreen

--- src/Program1_Stacks_Parking_Garage.py
+++ src/Program1_Stacks_Parking_Garage.py
@@ -1,19 +1,3 @@
-"""
-Stacks Parking Garage PseudoCode:
- 1. Create a storage and set max capacity
-    *10 Max Capacity
- 2. Give out option for the user to:
-    *Park the Car
-      - Insert the Car to the Stack
-    *Depart the Car
-      - Remove the Car from the Stack
-    *View Car
-      - View the Car, displaying its details
-    *Exit
-      -Exit the program
- 3. Create a program that organizes the Car using Stacks
-    *LIFO(Last in First Out)
-"""
 import pygame                 # Graphics and UI
 import sys                    # Exit program
 import os                     # File path handling
@@ -23,7 +7,7 @@ pygame.init()                 # Initialize pygame
 
 # ==================== WINDOW ====================
 WIDTH, HEIGHT = 1000, 700
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN)
 pygame.display.set_caption("Stack Parking Garage Simulation")
 
 CLOCK = pygame.time.Clock()
@@ -343,3 +327,6 @@ while running:
     CLOCK.tick(60)
 
 pygame.quit()
+
+if __name__ == "__main__":
+    pass  # The main loop runs automatically when the script is executed
